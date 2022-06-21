0be47774faa5f73423d24fb9469e9cb12bb9113b
Orchem <amiablechibykes@gmail.com>
2022-06-20 17:44:40 -0700

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-def raise_exception():
-    raise TypeError
+"""
+    prints x elements of a list.
+
+    """
+
+
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    for x in range(0, x):
+        try:
+            print(my_list[x], end="")
+            nb += 1
+        except:
+            break
+    print("")
+    return(nb)
